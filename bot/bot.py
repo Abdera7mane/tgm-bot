f9c50cde9eb912d124058efa4ee38a19b3041864
rachida0001 <103538352+rachida0001@users.noreply.github.com>
2022-08-13 00:44:33 +0100

    suggest command
    1- connect the bot to a firebase db
    2- add a command where users suggest a TGM subject. Each subject should have the following fields: title (string), status (PENDING, VALID, HANDLED), resources(array of string). When the subject is added by a user, it’s status is PENDING by default.

--- bot/bot.py
+++ bot/bot.py
@@ -3,10 +3,37 @@ import hikari
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
-intents = hikari.Intents.GUILD_VOICE_STATES | hikari.Intents.GUILD_SCHEDULED_EVENTS
-intents ^= 
+import lightbulb
 
-bot = hikari.GatewayBot(token=os.getenv("TOKEN"), intents=hikari.Intents.all())
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db 
+
+from config import *
+
+cred = credentials.Certificate(firebase_config)
+firebase_admin.initialize_app(cred, {'databaseURL': dbUrl_config})
+
+bot = lightbulb.BotApp(
+    token=Token,
+    default_enabled_guilds=(id)
+)
+
+ref = db.reference('/')
+
+ref = db.reference('subjects')
+
+@bot.command
+@lightbulb.option('suggestion', 'write a suggestion', type=str)
+@lightbulb.command('suggest', 'suggest a TGM subject')
+@lightbulb.implements(lightbulb.SlashCommand)
+async def suggestion(ctx):
+    ref.push({
+    'title' : ctx.options.suggestion,
+    'status' : 'PENDING',
+    'resources' : []
+    })
+    await ctx.respond("Your suggestion is in th db")
+
 
 bot.run()
